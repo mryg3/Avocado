4c794217bf765ce8ea57aafafabfaf8afd4d64d4
mryg3 <zhadan.antonina@gmail.com>
2024-01-19 13:20:36 +0100

    Create two plots

--- ggg.py
+++ ggg.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Replace 'your_file.csv' with the actual file path
 file_path = '/Users/antoninazhadan/Documents/programming/Avocado_dataset.csv'
@@ -73,3 +74,21 @@ avocado_data_cleaned = avocado_data_cleaned.drop(['plu4046', 'plu4225', 'plu4770
 
 st.subheader('Cleaned Data: ')
 st.write(avocado_data_cleaned)
+
+#Show some interesting plots
+
+#Distribution of Average Price
+plt.figure(figsize=(10, 8))
+plt.title('Distribution of AveragePrice')
+plt.hist(dataset['AveragePrice'], bins = 20, color='green',density=True)
+plt.xlabel('Average Price')
+plt.ylabel('Density')
+plt.show()
+
+#TotalBags and AveragePrice
+plt.figure(figsize=(10, 8))
+plt.title('TotalBags and AveragePrice')
+plt.scatter(dataset['TotalBags'], dataset['AveragePrice'], color= 'brown')
+plt.xlabel('Total Bags')
+plt.ylabel('Average Price')
+plt.show()
