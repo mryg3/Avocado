c935aee4e5f3f3d6a27a2bc86dca4509150e5726
mryg3 <zhadan.antonina@gmail.com>
2024-01-22 15:42:41 +0100

    making everything looks beutiful

--- ggg.py
+++ ggg.py
@@ -3,13 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Replace 'your_file.csv' with the actual file path
+# Downloading my file
 file_path = '/Users/antoninazhadan/Documents/programming/Avocado_dataset.csv'
 
-# Use pandas to read the CSV file
+# This helps to read the file
 dataset = pd.read_csv(file_path)
 
-# Now 'dataset' contains the data from the CSV file
+# Now i have my dataset
 print(dataset.head())
 
 import streamlit as st
@@ -23,12 +23,8 @@ st.image("/Users/antoninazhadan/Documents/programming/cute_avocado.png", caption
 st.caption('Designed by Antonina Zhadan')
 
 # Display the dataset
-st.subheader('Avocado Dataset:')
+st.header('Avocado Dataset:')
 st.dataframe(df)
-
-print(f'Name of Columns is: \n {dataset.columns}')
-print('Columns in dataset: '.format(dataset.shape[1]))
-print('Raws in dataset:'.format(dataset.shape[0]))
 
 # First five rows of my dataset
 dataset.head()
@@ -39,30 +35,35 @@ dataset.tail()
 #showing the info of each column in my dataset
 dataset.info()
 
-#see the names of columns
+#let's see what columns I have
+st.write(f'Name of Columns is: ')
 dataset.columns
 
 #rename the columns
-
 dataset.rename(columns={'SmallBags':'Small bags', 'LargeBags':'Large bags', 'XLargeBags':'XL bags'}, inplace=True)
 
-# Check for and count duplicated rows
+# Number of duplicated rows
 duplicate_count = dataset.duplicated().sum()
-print(f"Number of duplicated rows: {duplicate_count}")
+st.header("Number of Duplicated Rows:")
+st.write(f"There are {duplicate_count} duplicated rows in the dataset.")
 
 # Summary statistics
-dataset.describe().T
+columns_to_display = ['AveragePrice', 'TotalVolume', 'Small bags', 'Large bags', 'XL bags']
+st.header("Summary Statistics:")
+st.write(dataset[columns_to_display].describe().T)
 
 
-#DATA CLEANING 
+
+######################################################### DATA CLEANING ################################################################### 
 
 st.title('DATA CLEANING')
 
-# Display information about null values before cleaning
+# Let's see information about null values
 st.header("Null values before cleaning: ")
 st.write(dataset.isnull().sum())
 
-# Drop rows with any null values
+# Now, I want to drop rows with any null values
+st.text('droped the null values')
 avocado_data_cleaned = dataset.dropna()
 
 # Display information about null values after cleaning
@@ -72,52 +73,29 @@ st.write(avocado_data_cleaned.isnull().sum())
 # Drop duplicates
 avocado_data_cleaned = avocado_data_cleaned.drop_duplicates()
 
-#Display columns with missing data
+#I want to see the columns with missing data
 col_with_missing_data = dataset.columns[dataset.isnull().any()]
 st.header('Columns with missing data: ')
 st.write(col_with_missing_data)
 
-#Fill the missing columns with data using mean
+#Filling the missing columns with data using mean
 avocado_data_cleaned['Small bags'] = avocado_data_cleaned['Small bags'].fillna(avocado_data_cleaned['Small bags'].mean())
 avocado_data_cleaned['Large bags'] = avocado_data_cleaned['Large bags'].fillna(avocado_data_cleaned['Large bags'].mean())
 avocado_data_cleaned['XL bags'] = avocado_data_cleaned['XL bags'].fillna(avocado_data_cleaned['XL bags'].mean() )
 
-#Drop unecessary columns
+#I thought and decided to drop some unecessary columns
 avocado_data_cleaned = avocado_data_cleaned.drop(['plu4046', 'plu4225', 'plu4770', 'TotalVolume', 'type'], axis=1)
 
 st.subheader('Cleaned Data: ')
 st.write(avocado_data_cleaned)
 
-#Show some interesting plots
 
-#Distribution of Average Price
-#fig, ax = plt.subplots(figsize=(10, 8))
-#ax.title('Distribution of AveragePrice')
-#ax.hist(dataset['AveragePrice'], bins = 20, color='green',density=True)
-#ax.xlabel('Average Price')
-#ax.ylabel('Density')
-#st.pyplot(fig)
 
-#TotalBags and AveragePrice
-#fig, ax = plt.subplots(figsize=(10, 8))
-#ax.title('TotalBags and AveragePrice')
-#ax.scatter(dataset['TotalBags'], dataset['AveragePrice'], color= 'brown', alpha=0.5) #alpha allows to see overlapping points easily
-#ax.xlabel('Total Bags')
-#ax.ylabel('Average Price')
-#st.pyplot(fig)
+##################################################### Show some interesting plots ##########################################################
 
-# Calculate the mean AveragePrice by Region
-#average_price_by_region = dataset.groupby('region')['AveragePrice'].mean()
 
-# Plot the bar plot using Pandas plotting
-#fig, ax = plt.subplots(figsize=(10, 8))
-#average_price_by_region.plot(kind='bar', color='yellow')
-#ax.title('AveragePrice by Region')
-#ax.xlabel('Region')
-#ax.ylabel('Average Price')
-#st.pyplot(fig)
 
-# Distribution of Average Price
+# Let's see the distribution of Average Price
 if st.button('Show Distribution of Average Price chart'):
     fig, ax = plt.subplots(figsize=(4, 4))
     ax.hist(dataset['AveragePrice'], bins=20, color='green', density=True)
@@ -126,7 +104,7 @@ if st.button('Show Distribution of Average Price chart'):
     ax.set_ylabel('Density')
     st.pyplot(fig)
 
-# TotalBags and AveragePrice
+# Now the chart TotalBags and AveragePrice
 if st.button('Show TotalBags and AveragePrice chart'):
     fig, ax = plt.subplots(figsize=(4, 4))
     ax.scatter(dataset['TotalBags'], dataset['AveragePrice'], color='brown', alpha=0.5)
@@ -135,7 +113,7 @@ if st.button('Show TotalBags and AveragePrice chart'):
     ax.set_ylabel('Average Price')
     st.pyplot(fig)
 
-# Calculate the mean AveragePrice by Region
+# And now I want to calculate the mean AveragePrice by Region
 average_price_by_region = dataset.groupby('region')['AveragePrice'].mean()
 
 # AveragePrice by Region
@@ -147,14 +125,18 @@ if st.button('AveragePrice by Region chart'):
     ax.set_ylabel('Average Price')
     st.pyplot(fig)
     
-#Exploring the data
+    
+    
+####################################################### More Exploration of the data ##############################################################
+
+
 
 st.title('Exploring more data:')
 
-# Count occurrences of each region
+# Exploring each region
 region_counts = dataset['region'].value_counts()
 
-# Display the count of each region
+# Let's display the count of each region (first 10 and last 10)
 st.header('First 10 Region Counts')
 st.table(region_counts.head(10))
 
@@ -163,76 +145,84 @@ st.table(region_counts.tail(10))
 
 
 #I want to see the sum all bags
-# Create a figure and axis
 fig, ax = plt.subplots(figsize=(10, 4))
 
-# Calculate the sum of each bag type across all rows
+# Let's calculate the sum of each bag type in all rows
 total_sum_s_bags = dataset['Small bags'].sum()
 total_sum_l_bags = dataset['Large bags'].sum()
 total_sum_XL_bags = dataset['XL bags'].sum()
 
-# List of bag types and their corresponding sums
+# List of bag types and their sums
 bag_types = ['Small bags', 'Large bags', 'XL bags']
 sums = [total_sum_s_bags, total_sum_l_bags, total_sum_XL_bags]
 
-# Plot the bar chart
+# Plot the bar chart by Total sum of each bag type
 bars = ax.bar(bag_types, sums, color=['red', 'blue', 'black'])
-ax.set_title('Total Sum of Each Bag Type')  # Corrected line
+ax.set_title('Total Sum of Each Bag Type')  
 ax.set_xlabel('Bag Types')
 ax.set_ylabel('Total Sum')
 
-# Display the chart using st.pyplot() based on checkbox state
+# Let's display the chart using st.pyplot and creating a checkbox
 show_chart = st.checkbox('Check me to see the chart of total sum of all bags')
 if show_chart:
     st.pyplot(fig)
 else:
-    st.write('Bar chart is hidden. Click the checkbox to show it.')
+    st.write('Chart is hidden. Click me to show it.')
 
+#Let's convert "Data" into a datetime format
 dataset['Date'] = pd.to_datetime(dataset['Date'])
-# Group by 'Date' and calculate the average price
+#Now group by "Date" and calculate the average price
 average_price_over_time = dataset.groupby('Date')['AveragePrice'].mean()
 
-# Create a figure and axis
 fig, ax = plt.subplots(figsize=(18, 10))
-
-# Plot the line chart
+# Plot the line chart 
 ax.plot(average_price_over_time.index, average_price_over_time.values, color='blue', marker='o')
 ax.set_title('Average Price Over Time')
 ax.set_xlabel('Date')
 ax.set_ylabel('Average Price')
 
-# Rotate x-axis labels for better visibility
-plt.xticks(rotation=45, ha='right')
+st.title('Average Price Over Time')
+
+with st.container():
+    selected_year = st.radio("Select Year", sorted(dataset['Date'].unique()))
+    st.write("")  # Adding a space to separate the radio button from the plot
+
+# Filter the data based on the selected year
+filtered_data = dataset[dataset['Date'].dt.year == selected_year]
+
+# Plot the line chart
+fig, ax = plt.subplots(figsize=(18, 10))
+ax.plot(filtered_data['Date'], filtered_data['AveragePrice'], color='blue', marker='o')
+ax.set_title(f'Average Price Over Time - {selected_year}')
+ax.set_xlabel('Date')
+ax.set_ylabel('Average Price')
 
 # Display the chart using st.pyplot()
 st.pyplot(fig)
 
 
+
+# In order to see better the chart I'm Rotateting x-labels for better visibility
+plt.xticks(rotation=45, ha='right')
+st.pyplot(fig)
 
 # Print data types of all columns
 st.write("Data Types of Columns:")
 st.write(dataset.dtypes)
 
-
-# Select only numeric columns for correlation
-dataset_numeric = dataset.select_dtypes(include=['float64', 'datetime64[ns]'])
-# Display the correlation heatmap
-st.header('Correlation Heatmap')
-fig, ax = plt.subplots(figsize=(10, 8))
-heatmap = sns.heatmap(dataset_numeric.corr(), annot=True)
-st.pyplot(fig)
-
-
-
-# Create a figure and axis
 fig, ax = plt.subplots(figsize=(10, 6))
 
-# Plot the pie chart
+# Plot the pie chart by S S bags, L bags and XL bags
 ax.pie([total_sum_s_bags, total_sum_l_bags, total_sum_XL_bags], labels=bag_types, autopct='%.2f%%', startangle=90, shadow=True)
-
-# Set chart title and legend
 ax.set_title('Distribution of Bag Types', fontweight='bold', fontsize=14)
 ax.legend()
-
-# Display the chart using st.pyplot()
 st.pyplot(fig)
+
+# Making correlation (using only numerical values)
+dataset_numer = dataset.select_dtypes(include=['float64', 'datetime64[ns]'])
+# Let's see the correlation heatmap
+st.header('Correlation Heatmap')
+fig, ax = plt.subplots(figsize=(10, 8))
+heatmap = sns.heatmap(dataset_numer.corr(), annot=True)
+st.pyplot(fig)
+
